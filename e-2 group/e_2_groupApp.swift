f3504b764c2b1b8017f1883b482527c214ea3518
Riham <rihamoham3d@gmail.com>
2023-06-26 10:59:36 -0700

    Initial Commit

---
+++ e-2 group/e_2_groupApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_2_groupApp.swift
+//  e-2 group
+//
+//  Created by Riham Mohamed on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct e_2_groupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
