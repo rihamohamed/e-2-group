943d5dd586b4c5a7215cb7b08c25116ca4cc55af
Divya Saha <saha.divs@gmail.com>
2023-06-28 13:19:53 -0700

    Welcome page test 2 Image

--- e-2 group/ContentView.swift
+++ e-2 group/ContentView.swift
@@ -11,29 +11,32 @@ struct ContentView: View {
     @State private var name = ""
     @State private var textTile = "What ur name"
     var body: some View {
+        
         VStack {
             Text(textTile)
             TextField("", text: $name)
-            .multilineTextAlignment(.center)
-            .font(.title)
-            .border(Color.indigo, width: 3)
-            .padding()
-            
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.indigo, width: 3)
+                .padding()
+        }
+          
+        VStack {
             Button {
-                    
+                textTile = "Welcome \(name)!"
             } label: {
                 Image("Scholar")
-
             }
+        }
             
 
             
             
             
         }
-        .padding()
+        
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
